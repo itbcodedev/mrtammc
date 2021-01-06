2b616f1e44b2c7876c6d7df27164c01f4fd12c93
sawangpong@itbakey <sawangpong@itbakery.net>
2021-01-06 21:26:26 +0700

    commit fix parking icon

--- mrtammc/src/app/auth/parking/parking.component.ts
+++ mrtammc/src/app/auth/parking/parking.component.ts
@@ -8,42 +8,37 @@ declare let L;
 @Component({
   selector: 'app-parking',
   templateUrl: './parking.component.html',
-  styleUrls: ['./parking.component.scss']
+  styleUrls: ['./parking.component.scss'],
 })
 export class ParkingComponent implements OnInit {
+  @ViewChild('map', { static: true })
+  public mapElement: ElementRef;
 
-  @ViewChild("map", { static: true })
-  public mapElement: ElementRef
-
-  @ViewChild("mapdiv", { static: true })
+  @ViewChild('mapdiv', { static: true })
   public mapDiv: ElementRef;
 
   parkingForm: FormGroup;
 
-  map
-  height
-  latitude
-  longitude
-  controllerLayer
-  baseLayers
-  defaultColDef
-  columnDefs
-  rowData
+  map;
+  height;
+  latitude;
+  longitude;
+  controllerLayer;
+  baseLayers;
+  defaultColDef;
+  columnDefs;
+  rowData;
 
-
-
-
-  api
-  columnApi
-  userToBeEditedFromParent
-
+  api;
+  columnApi;
+  userToBeEditedFromParent;
 
   constructor(
     private fb: FormBuilder,
     private toastr: ToastrService,
     private parking: ParkingService,
-    public ngxSmartModalService: NgxSmartModalService) {
-
+    public ngxSmartModalService: NgxSmartModalService
+  ) {
     this.parkingForm = this.fb.group({
       code: ['', Validators.required],
       name: ['', Validators.required],
@@ -51,10 +46,10 @@ export class ParkingComponent implements OnInit {
       longitude: ['', Validators.required],
       image: ['', Validators.required],
       icon: ['', Validators.required],
-      capacity: ['', Validators.required]
-    })
+      capacity: ['', Validators.required],
+    });
 
-    this.height = 775 + "px";
+    this.height = 775 + 'px';
   }
 
   ngOnInit() {
@@ -62,49 +57,63 @@ export class ParkingComponent implements OnInit {
     this.map = L.map(this.mapElement.nativeElement).setView(latLon, 12);
 
     const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    const osmAttrib = '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-    const landUrl = 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
-    const thunAttrib = '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const osmAttrib =
+      '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const landUrl =
+      'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
+    const thunAttrib =
+      '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
     const osmMap = new L.tileLayer(osmUrl, { attribution: osmAttrib });
     const lightMap = new L.tileLayer(landUrl, { attribution: thunAttrib });
 
-    const googleStreets = new L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-      maxZoom: 20,
-      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
+    const googleStreets = new L.tileLayer(
+      'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+      {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      }
+    );
 
-    const googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
-      maxZoom: 20,
-      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
+    const googleHybrid = L.tileLayer(
+      'http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',
+      {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      }
+    );
 
-    const googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-      maxZoom: 20,
-      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
+    const googleSat = L.tileLayer(
+      'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+      {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      }
+    );
 
-    const googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}', {
-      maxZoom: 20,
-      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    });
+    const googleTerrain = L.tileLayer(
+      'http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+      {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+      }
+    );
 
     this.baseLayers = {
       'OSM Map': osmMap,
       'Light Map': lightMap,
-      'googleStreets': googleStreets,
-      'googleHybrid': googleHybrid,
-      'googleSat': googleSat,
-      'googleTerrain': googleTerrain
+      googleStreets: googleStreets,
+      googleHybrid: googleHybrid,
+      googleSat: googleSat,
+      googleTerrain: googleTerrain,
     };
-
 
     this.controllerLayer = L.control.layers(this.baseLayers);
     this.controllerLayer.addTo(this.map);
 
-
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution:
+        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.map);
 
     this.map.on('click', (e) => {
@@ -122,13 +131,18 @@ export class ParkingComponent implements OnInit {
       { headerName: 'Capacity', field: 'capacity', editable: true },
       { headerName: 'Latitude', field: 'latitude', editable: true },
       { headerName: 'Longitude', field: 'longitude', editable: true },
-    ]
+    ];
 
-    this.parking.getParking().subscribe(result => {
-      this.rowData = result
-    }, (error) => {
-      console.log(error)
-    })
+    this.parking.getParking().subscribe(
+      (result) => {
+        this.rowData = result;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    this.showmap();
   }
 
   // one grid initialisation, grap the APIs and auto resize the columns to fit the available space
@@ -142,60 +156,112 @@ export class ParkingComponent implements OnInit {
   onCellValueChanged(params: any) {
     this.rowData[params.rowIndex] = params.data;
     const obj = this.rowData;
-    console.log("141", obj);
+    console.log('141', obj);
   }
 
   refresh() {
-    this.parking.getParking().subscribe(result => {
-      this.rowData = result;
-    }, (error) => {
-      console.log(error);
-    });
+    this.parking.getParking().subscribe(
+      (result) => {
+        this.rowData = result;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  showmap() {
+    this.parking.getParking().subscribe(
+      (result) => {
+        this.rowData = result;
+
+        for (const c of this.rowData) {
+          console.log(c);
+          const lat = c.latitude;
+          const lng = c.longitude;
+          const marker = L.marker([lat, lng], {
+            icon: L.AwesomeMarkers.icon({
+              icon: 'car',
+              prefix: 'fa',
+              markerColor: 'blue',
+            }),
+            forceZindex: 100,
+          });
+
+          const customOptions = {
+            maxWidth: '500',
+            className: 'tg',
+          };
+          const popup = `<table class="tg">
+        <tr>
+          <th class="tg-0lax">รหัส<br></th>
+          <th class="tg-0lax">${c.code}<br></th>
+        </tr>
+        <tr>
+          <td class="tg-0lax">ชื่อสถานี</td>
+          <td class="tg-0lax">${c.name}</td>
+        </tr>
+        <tr>
+          <td class="tg-0lax">จำนวนที่จอด<br></td>
+          <td class="tg-0lax">${c.capacity} คัน</td>
+        </tr>
+        <tr>
+          <td colspan="2">
+          <img src="${c.image}" style="width:300px;height:200px;" />
+          </td>
+        </tr>
+      </table>`;
+          marker.bindPopup(popup, customOptions);
+          marker.addTo(this.map);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
   submitParkingData() {
     if (!this.parkingForm) {
-      this.toastr.error("กรอกข้อมูลผิดพลาด: ไม่ครบถ้วน")
+      this.toastr.error('กรอกข้อมูลผิดพลาด: ไม่ครบถ้วน');
       return false;
     } else {
       if (window.confirm('ต้องการที่จะ บันทึกข้อมูลหรือไม่?')) {
-        console.log("163",this.parkingForm.value)
+        console.log('163', this.parkingForm.value);
         const formData: any = new FormData();
 
-        formData.append('code', this.parkingForm.get('code').value)
-        formData.append('name', this.parkingForm.get('name').value)
-        formData.append('latitude', this.parkingForm.get('latitude').value)
-        formData.append('longitude', this.parkingForm.get('longitude').value)
-        formData.append('icon', this.parkingForm.get('icon').value)
-        formData.append('image', this.parkingForm.get('image').value)
-        formData.append('capacity', this.parkingForm.get('capacity').value)
-    
-        
+        formData.append('code', this.parkingForm.get('code').value);
+        formData.append('name', this.parkingForm.get('name').value);
+        formData.append('latitude', this.parkingForm.get('latitude').value);
+        formData.append('longitude', this.parkingForm.get('longitude').value);
+        formData.append('icon', this.parkingForm.get('icon').value);
+        formData.append('image', this.parkingForm.get('image').value);
+        formData.append('capacity', this.parkingForm.get('capacity').value);
+
         // post Formdata
-        this.parking.AddParking(formData).subscribe(res => {
-          console.log(res);
-          this.toastr.success(JSON.stringify(res))
-        }, (error) => {
-          console.log(error);
-          this.toastr.error(JSON.stringify(error))
-        });
+        this.parking.AddParking(formData).subscribe(
+          (res) => {
+            console.log(res);
+            this.toastr.success(JSON.stringify(res));
+          },
+          (error) => {
+            console.log(error);
+            this.toastr.error(JSON.stringify(error));
+          }
+        );
 
-        this.update()
+        this.update();
       }
-
     }
   }
-
-
 
   deleteParking() {
     if (window.confirm('ต้องการที่จะ บันทึกข้อมูลหรือไม่?')) {
       var selectedRows = this.api.getSelectedRows();
-      console.log(selectedRows)
+      console.log(selectedRows);
       if (selectedRows.length == 0) {
         this.toastr.error('กรุณาเลือก ข้อมูลที่ต้องการลบ', 'Error', {
-          timeOut: 3000
+          timeOut: 3000,
         });
         return;
       }
@@ -204,17 +270,18 @@ export class ParkingComponent implements OnInit {
       var res = this.api.updateRowData({ remove: selectedRows });
       console.log(res.remove[0].data);
       var id = res.remove[0].data._id;
-      this.parking.deleteParking(id).subscribe(result => {
-        console.log(result);
-        this.toastr.success(JSON.stringify(result))
-      }, (error) => {
-        console.log(error);
-        this.toastr.error(JSON.stringify(error))
-      });
+      this.parking.deleteParking(id).subscribe(
+        (result) => {
+          console.log(result);
+          this.toastr.success(JSON.stringify(result));
+        },
+        (error) => {
+          console.log(error);
+          this.toastr.error(JSON.stringify(error));
+        }
+      );
     }
-
   }
-
 
   // 1 Get updated row
   onSelectionChanged(event) {
@@ -222,18 +289,18 @@ export class ParkingComponent implements OnInit {
     this.userToBeEditedFromParent = selectedRows;
     console.log(this.userToBeEditedFromParent);
 
-    var selectedRowsString = "";
+    var selectedRowsString = '';
     selectedRows.forEach(function (selectedRow, index) {
       if (index > 5) {
         return;
       }
       if (index !== 0) {
-        selectedRowsString += ", ";
+        selectedRowsString += ', ';
       }
       selectedRowsString += selectedRow.id;
     });
     if (selectedRows.length >= 5) {
-      selectedRowsString += " - and " + (selectedRows.length - 5) + " others";
+      selectedRowsString += ' - and ' + (selectedRows.length - 5) + ' others';
     }
   }
 
@@ -242,46 +309,44 @@ export class ParkingComponent implements OnInit {
 
   onCellEditingStopped(e) {
     console.log(e.data);
-    this.parking.updateParking(e.data).subscribe(result => {
-      console.log(result);
-      this.toastr.success(JSON.stringify(result))
-    }, (error) => {
-      console.log(error);
-      this.toastr.error(JSON.stringify(error))
-    });
+    this.parking.updateParking(e.data).subscribe(
+      (result) => {
+        console.log(result);
+        this.toastr.success(JSON.stringify(result));
+      },
+      (error) => {
+        console.log(error);
+        this.toastr.error(JSON.stringify(error));
+      }
+    );
   }
 
-  //Get updated row  
+  //Get updated row
   onrowValueChanged(row) {
-    console.log("onrowValueChanged: ");
-    console.log("onrowValueChanged: " + row);
+    console.log('onrowValueChanged: ');
+    console.log('onrowValueChanged: ' + row);
   }
-
 
   // chage station icon
   onFileChange_image(event) {
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
-      console.log(file)
+      console.log(file);
       this.parkingForm.get('image').setValue(file);
     }
-
   }
 
   // chage station icon
   onFileChange_icon(event) {
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
-      console.log(file)
+      console.log(file);
       this.parkingForm.get('icon').setValue(file);
     }
-
   }
-
 
   update() {
     this.refresh();
     this.ngOnInit();
- } 
-
+  }
 }
